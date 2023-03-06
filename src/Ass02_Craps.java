7ada282b03b795623bd4fa6d9c7bb089cab597cf
James910066 <95539722+James910066@users.noreply.github.com>
2023-03-05 20:08:53 -0500

    Update Ass02_Craps.java
    Editing code because I had to clone this down to a new version of Intellij on my work laptop because my internet is out at home.

--- src/Ass02_Craps.java
+++ src/Ass02_Craps.java
@@ -4,127 +4,86 @@ public class Ass02_Craps
 {
     public static void main(String[] args)
     {
-        int point = 0; //other values that don't sum up to craps and natural. Keep rolling dice
+        //Declaration and assignment of all variables
+        int point = 0;
         int die1 = 0;
         int die2 = 0;
-        Random rnd = new Random();
-        int rndDice = rnd.nextInt(12) + 1;
+        Random rnd = new Random(); //Number generator for dice roll
         int diceTotal;
-        boolean stillPlaying = false;
-        String userResponse = "";
-        Scanner in = new Scanner(System.in);
-        String trash = "";
-        boolean rightInput = true;
+        boolean stillPlaying = false; //Variable for checking if user is still playing loop
+        String userResponse = ""; //Variable for user input
+        Scanner in = new Scanner(System.in); //Scanner for user input
+        String trash = ""; //Empty variable assignment for bad user input
+        boolean wrongInput = false; //Variable for validating user input loop
 
-           do
-           {   point = 0;
-               die1 = 0;
-               die2 = 0;
-               diceTotal = 0;
-               die1 = rnd.nextInt(6) + 1;
-               die2 = rnd.nextInt(6) + 1;
-               diceTotal = die1 + die2;
-
-
+           do //Start of the game loop
+           {
+               die1 = rnd.nextInt(6) + 1; //Set range for dice roll from 1 to 6
+               die2 = rnd.nextInt(6) + 1; //Set range for dice roll from 1 to 6
+               diceTotal = die1 + die2; //Variable assigned to sum of both dice
 
                //check for a craps roll
-                if (diceTotal == 2 || diceTotal == 3 || diceTotal == 12)
+                if (diceTotal == 2 || diceTotal == 3 || diceTotal == 12) //Check dice total for craps
                     {
-                        System.out.println("You rolled a " + die1 + " and a " + die2);
-                        System.out.println("You rolled a " + diceTotal + ": You crapped out!");
+                        System.out.println("You rolled a " + die1 + " and a " + die2); //Output statement for what user rolled on each die
+                        System.out.println("You rolled a " + diceTotal + ": You crapped out!"); //Output statement showing total of dice and player crapping out
                     }
-                else if (diceTotal == 7 || diceTotal == 11) //check for a natural roll
+                else if (diceTotal == 7 || diceTotal == 11) //check dice total for a natural
                     {
-                        System.out.println("You rolled a " + die1 + " and a " + die2);
-                        System.out.println("You rolled a natural " + diceTotal + ": You win!");
+                        System.out.println("You rolled a " + die1 + " and a " + die2); //Output statement for what user rolled on each die
+                        System.out.println("You rolled a natural: " + diceTotal + " You win!"); //Output statement showing total of dice and player rolling a natural
                     }
                 else //Dice roll is a point
                     {
-                        System.out.println("You rolled a " + die1 + " and a " + die2);
-                        System.out.println("You rolled a point " + diceTotal + ": Roll again");
-                        point = die1 + die2;
+                        System.out.println("You rolled a " + die1 + " and a " + die2); //Output statement for what user rolled on each die
+                        System.out.println("You rolled Point: " + diceTotal + " You get to Roll again"); //Output statement showing dice total and player rolling point
+                        point = die1 + die2; //Set point variable to new dice total
 
-                        while (diceTotal != 7 || diceTotal != point)
+                        while (diceTotal != 7 || diceTotal != point) //Loop while total dice roll is not 7 or equal to the point roller prior
                         {
-                            die1 = rnd.nextInt(6) + 1;
-                            die2 = rnd.nextInt(6) + 1;
-                            diceTotal = die1 + die2;
-                            if (diceTotal == point)
+                            die1 = rnd.nextInt(6) + 1; //Roll of die 1 with number generator
+                            die2 = rnd.nextInt(6) + 1; //Roll of die 2 with number generator
+                            diceTotal = die1 + die2; //Set variable to total of both dice
+                            if (diceTotal == point) //Check if player rolled to match point
                                 {
-                                    System.out.println("You rolled " + diceTotal + " again, You win!");
-                                    break;
+                                    System.out.println("You rolled " + diceTotal + " again, You win!"); //Output statement showing total that player rolled matching point and winning
+                                    break; //Jump out of inner loop for rolling point into outer loop to start game again where user is asked to play again
                                 }
-                            else if (diceTotal == 7)
+                            else if (diceTotal == 7) //check if player rolled a 7
                                 {
-                                    System.out.println("You rolled a " + diceTotal + "; You lose");
-                                    break;
+                                    System.out.println("You rolled a " + diceTotal + "; You lose"); //Output statment showing dice total and that they lost for rolling a 7
+                                    break; //Jump out of inner loop for rolling point into outer loop to start game again where user is asked to play again
                                 }
-                            else
+                            else //Player rolled other value that is not point or a 7
                                 {
-                                    System.out.println("You rolled a " + die1 + " and a " + die2);
-                                    System.out.println("You rolled a point " + diceTotal + ": Roll again");
-                                    point = die1 + die2;
+                                    System.out.println("You rolled a " + die1 + " and a " + die2); //Output statement showing what the player rolled for each die
+                                    System.out.println("You rolled a total of " + diceTotal + ". You get to Roll again"); //Output Statement showing total of dice rolled and telling user to roll again
                                 }
                         }
                     }
-
-
-                   System.out.println("Want to play again? [Y/N]"); //Prompt users to play game again or not
+                //Loop for checking user input
+               do { //Start of loop
+                   System.out.print("Want to play again? [Y/N] "); //Prompt users to play game again or not
                    userResponse = in.nextLine(); //User input assigned to boolean variable
 
-                   if (userResponse.equalsIgnoreCase("Y")) //Convert user input to either case
-                   {
-                       stillPlaying = true; //Change of boolean variable from false because user chose Y or y
-                   }
-                   else if(userResponse.equalsIgnoreCase("N"))
-                   {
-                       stillPlaying = false;
-                   }
-                   else
-                   {
-                       trash = in.nextLine();
-                       System.out.println("Bad input");
-                   }
+                   if (userResponse.equalsIgnoreCase("Y")) //Check if user want to play again
+                       {
+                           stillPlaying = true; //Change of boolean variable from false because user chose Y or y
+                           wrongInput = false; //Jump out of inner loop to outer loop for playing game again
+                       }
+                   else if(userResponse.equalsIgnoreCase("N")) //Check if user chose to quit game
+                       {
+                           stillPlaying = false; //Boolean variable changed to false for quiting game
+                           wrongInput = false; //Jump out of inner loop
+                       }
+                   else //Bad user input goes here
+                       {
+                           trash = in.nextLine(); //Set variable to bad user input for output statement
+                           wrongInput = true; //Change boolean variable so loop continues to ask for correct input
+                           System.out.println(trash + " is invalid choice. Choose [Y/N]"); //Output statement to notify user of bad input
+                       }
 
-
-
-
-              /* do //Start of Do While loop for testing playerA input
-               {
-                   System.out.println("Want to play again? "); //Prompt users to play game again or not
-                   if (in.hasNextInt()) //Check if user input is an integer and not string
-                   {
-                       trash = in.nextLine(); //Assignment of bad input from user
-                       System.out.println(trash + " is invalid input."); //Output statement of bad input to user
-                   }
-                   else //Other choice on good input
-                   {
-                       playerA = in.nextLine(); //Assignment of correct user input
-                       correctInputPlayerA = true; //Reassign variable condition to end loop
-                   }
-               } while (!correctInputPlayerA); //Boolean check for continuing user prompt*/
-
-               /* do {
-                    System.out.println("Want to play again? [Y/N]"); //Prompt users to play game again or not
-                    //userResponse = in.nextLine(); //User input assigned to boolean variable
-
-                    if (userResponse.equalsIgnoreCase("Y")) //Convert user input to either case
-                        {
-                            stillPlaying = true; //Change of boolean variable from false because user chose Y or y
-                        }
-                    else if (userResponse.equalsIgnoreCase("N"))
-                        {
-                            stillPlaying = false;
-                        }
-                    else
-                        {
-                            trash = in.nextLine();
-                            rightInput = false;
-                            System.out.println(userResponse + " is an invalid answer. Want to play again? [Y/N]");
-                        }
-                    } while (!rightInput);*/
-           }while (stillPlaying);
-
-
+               } while (wrongInput); //Boolean loop condition used to keep asking user for correct input
+           } while (stillPlaying); //Boolean loop condition stating true to keep playing until user chooses N
     }
 }
